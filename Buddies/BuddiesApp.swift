bc142ff5496db89c4621aa88f79fe60575192364
DavidEtoedia <etoedia@gmail.com>
2024-01-02 02:37:05 -0800

    Initial Commit

---
+++ Buddies/BuddiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuddiesApp.swift
+//  Buddies
+//
+//  Created by Inyene Etoedia on 02/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct BuddiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
